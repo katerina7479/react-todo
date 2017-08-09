38956c9c356ca0682b1762e657ef0d65a29f36e4
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-08-08 18:35:55 -0700

    Finish adding todos reducers

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,4 +1,8 @@
-export var searchTextReducer = (state = '', action) => {
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+
+export const searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
       return action.searchText;
@@ -13,5 +17,36 @@ export const showCompletedReducer = (state = false, action) => {
       return !state;
     default:
       return state;
+  }
+}
+
+export const todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined,
+        }
+      ]
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id == action.id) {
+          var newCompleted = !todo.completed;
+          return {
+            ...todo,
+            completed: newCompleted,
+            completedAt: newCompleted ? moment().unix() : undefined,
+          }
+        } else {
+          return todo
+        }
+      })
+    default:
+      return state
   }
 }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,26 +1,54 @@
 var expect = require('expect');
+var df = require('deep-freeze-strict');
 var reducers = require('Reducers');
+var moment = require('moment');
+
 
 describe('Reducers', () => {
-  describe('searchTextReducer', () => {
+  describe('Search text Reducer', () => {
     it('should set search text', () => {
       var action = {
         type: 'SET_SEARCH_TEXT',
         searchText: 'dog'
       };
-      var res = reducers.searchTextReducer('', action);
+      var res = reducers.searchTextReducer(df(''), df(action));
       expect(res).toEqual(action.searchText);
     });
   });
-  describe('show Completed', () => {
+  describe('Show Completed Reducer', () => {
     it('should toggle show Completed', () => {
       var action = {
         type: 'TOGGLE_SHOW_COMPLETED',
       };
-      var res = reducers.showCompletedReducer(true, action);
+      var res = reducers.showCompletedReducer(df(true), df(action));
       expect(res).toEqual(false);
     });
   });
+  describe('Todos reducer', () => {
+    it('should add new todos', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Thing I want to do'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+    it('should toggle todos', () => {
+      var todos = [
+        { id: 1, text: "Feed Dog", completed: false, createdAt: moment().unix() },
+        { id: 2, text: "Water Plants", completed: false, createdAt: moment().unix() },
+      ]
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(2);
+      expect(res[1].text).toEqual("Water Plants");
+      expect(res[1].completed).toEqual(true);
 
+    });
+  });
 
 });
