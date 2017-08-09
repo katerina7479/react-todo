4da343005bd97ef76c89f7b689444657e8887100
Katerina Hanson <katerina.hanson@stridehealth.com>
2017-08-09 10:21:49 -0700

    Store updates

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -11,5 +11,7 @@ export var configure = () => {
 
   var store = redux.createStore(reducer, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : f => f
-  ))
+  ));
+
+  return store;
 };
